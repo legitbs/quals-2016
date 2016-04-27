8c30e7e9e7325084a8e034d92c02763c549464ff
Vito Genovese <vito@legitbs.net>
2016-04-26 22:44:33 -0400

    start with stack

--- stack/poller/for-release/machine.py
+++ stack/poller/for-release/machine.py
@@ -6,27 +6,8 @@ import random
 
 class TemplateGenerator(Actions):
     def start(self):
-        self.read(delim='.\n')
-        self.read(delim='\n', expect='This implements a simple echo service\n')
+        1 + 1 #noop
 
-    def go(self):
-        value = None
-        values = [chr(x) for x in range(255)]
-        values.remove('\n')
-
-        while True:
-            size = random.randint(1, 1022)
-
-            value = ''.join(random.choice(values) for _ in range(size))
-
-            # don't include a POV
-            if value[:2] != 'AB':
-                break
-
-        value += '\n'
-
-        self.write(value)
-        self.read(length=len(value), expect=value)
-
-    def finish(self):
-        self.comment('nothing left to test')
+    def echo(self):
+        self.write("vito\n")
+        self.read(length=4, expect="vito")
--- stack/src/service.c
+++ stack/src/service.c
@@ -1,127 +1,88 @@
 #include <libcgc.h>
 #include "libc.h"
 
-#define STR0 "Hello "
-#define STR1 ".\nThis implements a simple echo service\n"
+#define GETS_BUF_LEN 16
+#define PRINT_BUF_LEN 16
+#define CANARY "vito"
+#define ECHO_BUF_LEN 24
 
-#define NAME_SIZE 4
-#define BUF_SIZE 1024
+int gets(char* dest) {
+  char buf[GETS_BUF_LEN];
 
-size_t printf(const char *format, ...);
+  int dest_cursor = 0;
 
-char * get_name(void *secret_page);
-size_t receive_line(int fd, char *buf, size_t size);
+  while (1) {
+    size_t got;
+    if (receive(STDIN, buf, GETS_BUF_LEN, &got) != 0) _terminate(-1);
 
-char * get_name(void *secret_page) {
-    char *buf;
-    const char *names[] = {"Matt", "Mark", "Luke", "John"};
-    unsigned int index;
-    int ret;
-    int i;
-   
-    index = ((int *)secret_page)[0] % (sizeof(names) / sizeof(names[0]));
+    for (int buf_cursor = 0; buf_cursor < got; buf_cursor++) {
+      if (buf[buf_cursor] == '\n') {
+        dest[dest_cursor + buf_cursor] = 0;
+        return dest_cursor + buf_cursor;
+      }
 
-    ret = allocate(NAME_SIZE + 1, 0, (void **) &buf);
-    if (ret != 0)
-        _terminate(1);
-
-    for (i = 0; i < NAME_SIZE; i++) {
-        buf[i] = names[index][i];
-    }
-
-    return buf;
-}
-
-size_t receive_line(int fd, char *buf, size_t size) {
-    char c;
-    size_t total = 0;
-    size_t read = 0;
-
-    while (1) {
-        if (receive(fd, &c, 1, &read) != 0) {
-            _terminate(1);
-        }
-
-        if (read == 0) {
-            return 0;
-        }
-
-        if (total >= size) {
-            return 0;
-        }
-
-        buf[total++] = c;
-
-        if (c == '\n') {
-            break;
-        }
-    }
-
-    return total;
-}
-
-void *copy(void *dst, const void *src, size_t size) {
-    char *dst_char = (char *)dst;
-    char *src_char = (char *)src;
-
-    while (size--)
-        *dst_char++ = *src_char++;
-
-    return dst;
-}
-
-
-int __attribute__((fastcall)) main(int secret_page_i, char *unused[]) {
-    void *secret_page = (void *)secret_page_i;
-    char buf[1024];
-    char *name;
-    int ret;
-    size_t size;
-    
-    name = get_name(secret_page);
-
-    ret = transmit_all(STDOUT, STR0, sizeof(STR0) - 1);
-    if (ret != 0)
-        _terminate(1);
-
-    ret = transmit_all(STDOUT, name, NAME_SIZE);
-    if (ret != 0)
-        _terminate(1);
-
-    ret = transmit_all(STDOUT, STR1, sizeof(STR1) - 1);
-    if (ret != 0)
-        _terminate(1);
-
-    ret = deallocate(name, NAME_SIZE + 1);
-    if (ret != 0)
-        _terminate(1);
-
-    while (1) {
-        size = receive_line(STDIN, buf, sizeof(buf) - 1);
-        if (size == 0)
-            _terminate(1);
-
-        buf[size] = 0;
-
-#ifndef PATCHED_1
-        if (buf[0] == 0x41 && buf[1] == 0x42 && buf[2] == 0x43) {
-            int (*func)(int);
-            copy(&func, buf+3, 4);
-            copy(&size, buf+7, 4);
-            transmit(STDERR, "type1\n", 6, NULL);
-            ret = (*func)(size);
-        }
+#ifdef PATCHED_1
+      if (dest_cursor + buf_cursor > ECHO_BUF_LEN) _terminate(-1);
 #endif
 
-#ifndef PATCHED_2
-        if (buf[0] == 0x41 && buf[1] == 0x42 && buf[2] == 0x44) {
-            transmit(STDERR, "type2\n", 6, NULL);
-            transmit_all(STDOUT, secret_page, 0x1000);
-        }
-#endif
-
-        ret = transmit_all(STDOUT, buf, size);
-        if (ret != 0)
-            _terminate(1);
+      dest[dest_cursor + buf_cursor] = buf[buf_cursor];
     }
+
+    dest_cursor += got;
+  }
+}
+
+void print(char* src) {
+  int src_len = 0;
+
+  while (1) {
+    if (src[src_len] == 0) break;
+    src_len++;
+  }
+
+  int total_sent = 0;
+
+  while(1) {
+    int remaining = src_len - total_sent;
+    int to_send = (PRINT_BUF_LEN > remaining) ? PRINT_BUF_LEN : remaining;
+
+    size_t did_send;
+
+    if (transmit(STDOUT, src + total_sent, to_send, &did_send) != 0) {
+      _terminate(-1);
+    }
+
+    total_sent += did_send;
+
+    if (total_sent >= src_len) return;
+  }
+}
+
+int streq(char* left, char* right, int count) {
+  for (int cursor = 0; cursor < count; cursor++) {
+    char l = left[cursor];
+    char r = right[cursor];
+
+    if (l != r) return 0;
+  }
+
+  return 1;
+}
+
+void echo() {
+  char canary[] = CANARY;
+  char buf[ECHO_BUF_LEN];
+
+  gets(buf);
+  print(buf);
+
+  if (!streq(canary, CANARY, 4)) {
+    print("hacking detected, see ya");
+    _terminate(-1);
+  }
+}
+
+int main() {
+  echo();
+  return 0;
 }
