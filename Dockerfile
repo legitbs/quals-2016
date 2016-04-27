ac5dcc8884142ea341ab3ac34b2c4a99cbbd7627
Vito Genovese <vito@legitbs.net>
2016-04-26 21:15:37 -0400

    use cgc service template

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM ubuntu:xenial
-
-RUN apt-get update; apt-get upgrade -y
-RUN apt-get install -y build-essential gdb libc-dbg
-
-WORKDIR /app
-
-CMD ["/bin/bash"]
--- stack/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-gcc\
-    -ansi -zexecstack -fno-stack-protector -no-pie -g \
-    -D 'CANARY="asdf"' -D USER_BUF_LEN=40 -D PAD_BUF_LEN=10 \
-    -o main \
-    main.c
