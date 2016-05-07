db32e6618e6d46238903f59948a0e77acb50262a
Vito Genovese <vito@legitbs.net>
2016-05-07 19:50:53 -0400

    runners for medium and hard

---
+++ scramble-stack/runner.py
@@ -0,0 +1,40 @@
+import random
+from os import environ, listdir, path
+from sys import exit
+from subprocess import Popen, PIPE
+import signal
+from base64 import b64decode
+
+def alarm_handler(signum, frame):
+    print "timed out, sorry"
+    exit(-1)
+signal.signal(signal.SIGALRM, alarm_handler)
+
+input_timeout = int(environ.get('INPUT_TIMEOUT', 15))
+crash_timeout = int(environ.get('CRASH_TIMEOUT', 5))
+
+chall_path = "./tmp/gen/cb"
+all_challs = set(listdir(chall_path))
+
+picked = random.sample(all_challs, 10)
+
+print "send your crash string as base64, followed by a newline"
+
+for c in picked:
+    print c
+    signal.alarm(input_timeout)
+    crasher = b64decode(raw_input())
+    signal.alarm(0)
+
+    signal.alarm(crash_timeout)
+    proc = Popen(path.join(chall_path, c),
+                 stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    (out, err) = proc.communicate(crasher)
+    signal.alarm(0)
+
+    if proc.returncode != -signal.SIGSEGV:
+        print "didn't segfault, sorry"
+        exit(-1)
+
+print "The flag is: {}".format(
+    environ.get('FLAG', "do you want a thousand bandages gruanfir3"))
